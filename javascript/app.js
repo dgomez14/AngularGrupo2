a0396101e3fcb05d6d256f4bfb6146e7d497b8df
davalgom <davalgom@bancolombia.com.co>
2020-07-17 19:09:35 -0500

    Javascript

---
+++ javascript/app.js
@@ -0,0 +1,45 @@
+number = 100;
+console.log(typeof number === 'number');
+number = 3.14;
+console.log(typeof number === 'number');
+number = Infinity;
+console.log(typeof number === 'number');
+number = NaN;
+console.log(typeof number === 'number');
+
+
+var user = 'David';
+let username = 'davalgom';
+let email = `${ username }@bancolombia.com.co`;
+console.log(email);
+email = username + '@bancolombia.com.co';
+console.log(email);
+
+
+username = true;
+username = false;
+
+let obj = undefined;
+console.log(typeof obj);
+
+obj = {
+    attr: 100,
+    'nombre de usuario': 'David'
+};
+
+
+console.log(typeof obj);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(Array.isArray(numbers));
+
+let func = function (a, b) {
+    return a.concat(b);
+};
+
+func = (a, b) => a.concat(b);
+
+console.log(func('Hola ', 'Mundo'));
+
+console.log([1, 2, 3] + 'Hola');
+
---
+++ javascript/arrays.js
@@ -0,0 +1,32 @@
+const frameworks = ['Spring'];
+frameworks.push('Angular');
+frameworks.push('Laravel', 'Django');
+
+// console.log(frameworks.pop());
+console.log(frameworks.length);
+
+const find = 'Angular';
+
+const found = frameworks.find(elem => elem === find);
+
+console.log(frameworks.filter(elem => {
+    return elem.includes('a');
+}));
+
+console.log(frameworks.map(el => el.concat('1')));
+
+const numbers = [2, 4, 4, 6, 8, 9];
+
+console.log(numbers);
+const reduced = numbers.reduce((acc, next) => {
+    console.log(acc, next);
+    return acc + next;
+});
+console.log(reduced);
+
+console.log('Every', numbers.every(n => n % 2 === 0));
+console.log('Some', numbers.some(n => n % 2 === 0));
+console.log('Sort', numbers.sort((a, b) => b - a));
+
+console.log(found);
+console.log(frameworks);
